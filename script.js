10e011cca2f1e0888d0f83f90ec0809671845a76
Caio Labella <caiolabella2016@gmail.com>
2024-11-21 18:54:26 -0300

    ajustado site para atender novos projetos

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleMode() {
   //trocar imagem
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "assets/avatar-lightmode.png")
+    img.setAttribute("src", "assets/avatar.png")
   } else {
     img.setAttribute("src", "assets/avatar.png")
   }
